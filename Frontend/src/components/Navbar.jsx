8193f2c0d0dcc71e33250cf1a050030d19db08a2
anushreebhattacharya <anushreebhattacharya606@gmail.com>
2026-09-12 13:55:25 +0530

    Corrected case

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../Assets/Logo.jpg'
+import Logo from '../assets/Logo.jpg'
 import { Link } from 'react-router-dom'
 import { UserCircle } from 'lucide-react'
 
