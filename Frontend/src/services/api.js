3a5cf44f2b5dc229bd0777b1320e1ce335b6c126
anushreebhattacharya <anushreebhattacharya606@gmail.com>
2026-09-12 13:49:18 +0530

    Prepare frontend for deployment

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5002/api",
+  baseURL: "https://ai-based-placement-predictor-1.onrender.com/api",
 });
 
 // Attach JWT automatically
